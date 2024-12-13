f86933add7f3037da405d9722c9e1ee5eab59f7c
Martin Grenfell <martin.grenfell@gmail.com>
2024-12-13 12:10:59 +0000

    play api wip

---
+++ spec/requests/api/v1/creatures/play_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+describe "Creature PlayTime resources" do
+  describe "POST /api/v1/creatures/:id/play_times" do
+    it "plays with creature and decreases its loneliness" do
+      creature = create(:creature, loneliness: 10)
+
+      post api_v1_creature_play_times_path(creature.uuid)
+      expect(response).to be_successful
+
+      expect(creature.reload.loneliness).to be < 10
+    end
+
+    it "explodes if the creature doesn't exist" do
+      dish = create(:dish)
+
+      post api_v1_creature_play_times_path("non-existing-creature-id", dish_id: dish.uuid)
+      expect(response).to be_not_found
+    end
+  end
+end
